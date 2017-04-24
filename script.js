38fdd7f55ab0c22e9540d263325a6b8be90eb44c
shabbir1993 <syed_shabbir_haider@hotmail.com>
2017-04-24 17:18:03 +0500

    Got rid of global variables in script via IIFE

--- script.js
+++ script.js
@@ -2,72 +2,83 @@
  * Created by Shabbir on 4/16/2017.
  */
 //Public Variables
-var long = 0; //stores longitude value
-var lat = 0; //stores latitude value
-var  main, place , wind, wind_speed, weather_info, icon, title, description, temp, country;
-var url = "http://openweathermap.org/img/w/";
+(function() {
+    var long = 0; //stores longitude value
+    var lat = 0; //stores latitude value
+    var  main, place , wind, wind_speed, weather_info, icon, title, description, temp, country;
+    var url = "http://openweathermap.org/img/w/";
 
-
-getLocation();
-
-
-
-/******************Function Definitions*******************/
-
-function getDatafromJSON(jsonObj) {
-    weather_info = jsonObj['weather'];
-    country = jsonObj['sys'];
-    country = country['country'];
-    icon = weather_info[0]['icon'];
-    icon += ".png";
-    url += icon;
-    title = weather_info[0]['main'];
-    description = weather_info[0]['description'];
-    place = jsonObj['name'];
-    main = jsonObj['main'];
-    wind = jsonObj['wind'];
-    wind_speed = wind['speed'];
-    temp = main['temp'];
-    temp = temp-275; //converting Kalvin to degree Celcius
-    temp = temp.toFixed(0);
-}
-
-function render(){
-
-    $("#place").html(place + " , " + country);
-    $("#temperature").html(temp + "&#8451");
-    $("#description").html(description);
-    $("#weatherIcon").html("<img src= "+url+">");
-}
+    getLocation();
 
 
 
+    /******************Function Definitions*******************/
 
-function getLocation(){
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
-            CallApi(long,lat);
+    function getDatafromJSON(jsonObj) {
+        weather_info = jsonObj['weather'];
+        country = jsonObj['sys'];
+        country = country['country'];
+        icon = weather_info[0]['icon'];
+        icon += ".png";
+        url += icon;
+        title = weather_info[0]['main'];
+        description = weather_info[0]['description'];
+        place = jsonObj['name'];
+        main = jsonObj['main'];
+        wind = jsonObj['wind'];
+        wind_speed = wind['speed'];
+        temp = main['temp'];
+        temp = temp-275; //converting Kalvin to degree Celcius
+        temp = temp.toFixed(0);
+    }
+
+    function render(){
+
+        $("#place").html(place + " , " + country);
+        $("#temperature").html(temp + "&#8451");
+        $("#description").html(description);
+        $("#weatherIcon").html("<img src= "+url+">");
+    }
+
+
+
+
+    function getLocation(){
+        try {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    long = position.coords.longitude;
+                    lat = position.coords.latitude;
+                    CallApi(long,lat);
+                });
+            }
+        }
+
+        catch(ex){
+            alert(ex + "No connection...");
+        }
+
+        /*else {
+            alert("This is displayed when no connection");
+        }*/
+    }
+
+    function CallApi(long, lat){
+        $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat="+ lat +"&lon="
+            + long + "&appid=45923afd28277f058ece91425ebdbc7b"
+            , function (response, status) {
+                if(status == "success") {
+                    getDatafromJSON(response);
+                    render();
+                }
+
+                else if(status == "timeout"){
+                    alert("Something Wrong with the Connection");
+                    $("#weather").html("Low or No Connectivity...");
+                }
         });
     }
-}
-
-function CallApi(long, lat){
-    $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat="+ lat +"&lon="
-        + long + "&appid=45923afd28277f058ece91425ebdbc7b"
-        , function (response, status) {
-            if(status == "success") {
-                getDatafromJSON(response);
-                render();
-            }
-
-            else if(status == "timeout"){
-                alert("Something Wrong with the Connection")
-                $("#weather").html("Low or No Connectivity...")
-            }
-    });
-}
+}());
 
 
 
